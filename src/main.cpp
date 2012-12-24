20d0f04c12881f827794348634a59b96666c8b94
oangelo <oangelo@gmail.com>
2012-12-24 17:07:53 -0200

    Bifurcation Menu improve

--- src/main.cpp
+++ src/main.cpp
@@ -35,11 +35,15 @@ void help(){
     std::cout << " --parameters <real>[n]" << std::endl;
     std::cout << " --initial_conditions <real>[n]" << std::endl;
     std::cout << "options:" << std::endl;
+    std::cout << "  --print                          print the attractor on the screen" << std::endl;
     std::cout << "  --step <integer>                 number of steps to iterate before print the system properties" << std::endl;
     std::cout << "  --transient <integer>            number of steps to irerate before print anything" << std::endl;
     std::cout << "  --iterations <integer>           total number of iterations" << std::endl;
     std::cout << "  --lyapunov                       calculates the system lyapunov" << std::endl;
-    std::cout << "  --bifurcation <option> <integer> option: variable, parameter " << std::endl;
+    std::cout << "  --bifurcation <option> <integer> X <integer> Y <integer> value <real>" << std::endl;
+    std::cout << "                                   option: variable, parameter, X = variable to use as coordinate x, " << std::endl;
+    std::cout << "                                   Y = variable to use as coordinate y, " << std::endl;
+    std::cout << "                                   value = value were the trajectory corss Y, " << std::endl;
     std::cout << std::endl;
     std::cout << "double pendulum:" << std::endl;
     std::cout << "parameters: l1 l2 m1 m2 g" << std::endl;
@@ -104,12 +108,6 @@ int main( int argc , char * argv[]) {
                 lyapunov<Jacobian_RosslerFunction> (*model, iterations, transient, step, file_name);
                 return 0;
             }
-            for(unsigned i = 0; i < iterations; i++){
-                model->next();
-                if(i % step == 0)
-                    std::cout << *model << std::endl;
-            }
-
         }
 
         if(strcmp(argv[i], "--double_pendulum") == 0 or strcmp(argv[i], "-dp") == 0) {
@@ -139,7 +137,6 @@ int main( int argc , char * argv[]) {
                     variable[V_OMEGA2] = atof(argv[j + 4]);
                 }
             }
-
             model = new AdamsBashforth<DoublePendulumFunction>(variable,parameter,0.00001);
             if(strcmp(argv[i + 1], "--lyapunov") == 0) {
                 std::cerr << "#>> Calculating Lyapunov exponent" << std::endl;
@@ -147,32 +144,41 @@ int main( int argc , char * argv[]) {
                 lyapunov<Jacobian_DoublePendulumFunction> (*model, iterations, transient, step, file_name);
                 return 0;
             }
-            if(argc > i + 1)
-                if(strcmp(argv[i + 1], "--print") == 0) 
-                    for(unsigned i = 0; i < iterations; i++){
-                        model->next();
-                        if(i % step == 0)
-                            std::cout << *model << std::endl;
-                    }
-
-                
         }
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        
         if(strcmp(argv[i], "--bifurcations") == 0) {
             std::cerr << "#>> Calculating bifurcations" << std::endl;
+            type_data coordinate_value = 0.0;
+            int coordinate_x = 0, coordinate_y = 2;
+
             double control_parameter = std::numeric_limits<double>::signaling_NaN();
-            if(argc > i + 2){
-                if(strcmp(argv[i + 1], "variable") == 0) 
-                    control_parameter = model->get_variable(atoi(argv[i + 2]));
-                if(strcmp(argv[i + 1], "parameter") == 0) 
-                    control_parameter = model->get_parameter(atoi(argv[i + 2]));
+            for (int j = i; j < argc; ++j) {
+                if(strcmp(argv[j], "variable") == 0) 
+                    control_parameter = model->get_variable(atoi(argv[i + 1]));
+                if(strcmp(argv[j], "parameter") == 0)
+                    control_parameter = model->get_parameter(atoi(argv[i + 1]));
+                if(strcmp(argv[j], "X") == 0) 
+                    coordinate_x = atoi(argv[j + 1]);
+                if(strcmp(argv[j], "Y") == 0) 
+                    coordinate_y = atoi(argv[j + 1]);
+                if(strcmp(argv[j], "value") == 0) 
+                    coordinate_value = atoi(argv[j + 1]);
             }
 
-            type_data coordinate_value = 0.75;
-            int coordinate_x = 2, coordinate_y = 3;
             auto results = AttractorCrossAxis(*model, iterations, transient, coordinate_x, coordinate_y, coordinate_value);
             for(auto i: results)
                 std::cout << control_parameter << " " << i << std::endl;
         }
+        if(strcmp(argv[i], "--print") == 0) 
+                for(unsigned i = 0; i < iterations; i++){
+                    model->next();
+                    if(i % step == 0)
+                        std::cout << *model << std::endl;
+                }
+
     }
 
     if(model)
